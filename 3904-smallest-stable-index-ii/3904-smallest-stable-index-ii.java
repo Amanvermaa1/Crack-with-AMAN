eb0406320d514ed29ce00d0915567f7f0d47380e
Aman Verma <167960758+Amanvermaa1@users.noreply.github.com>
2026-09-05 13:58:22 +0530

    Time: 3 ms (100%), Space: 133.6 MB (5.34%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+         int n  =  nums.length ; 
+        int [] suffix = new int[n];
+        int min = nums[n-1];
+        for(int i = n-1 ; i >= 0 ; i--){
+            min = Math.min(min,nums[i]);
+            suffix[i] = min ;
+        }
+        int max = nums[0];
+        for(int i = 0 ; i< n; i++){
+            max = Math.max(max,nums[i]);
+            if(max- suffix[i] <= k) return i; 
+        }
+        return -1 ;
+    }
+}
